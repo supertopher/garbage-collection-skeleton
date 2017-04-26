04a620f186348e3ba2b02d40a394a552fd5fce88
Topher Lubaway <clubaway@pivotal.io>
2017-04-26 08:49:21 -0700

    Format skeleton to look more like it's intended function

--- Gemfile
+++ Gemfile
@@ -1,5 +1,5 @@
 source "https://www.rubygems.org"
-ruby "2.0.0"
+ruby "2.4.0"
 
 gem 'sinatra'
 gem 'shotgun'
--- public/application.js
+++ public/application.js
@@ -1,16 +1,3 @@
 $(function() {
-  $(".named-link").on("click", function(event){
-    event.preventDefault()
-    $.ajax({
-      url: "/cats"
-    }).done(function(data){
-      $(".all-the-things").html(data)
-    })
-  })
 
-
-  $(".all-the-things").on("click", (".brownbatlink"), function(event){
-    event.preventDefault()
-    alert("we bound that stuff. fo sho")
-  })
 });
